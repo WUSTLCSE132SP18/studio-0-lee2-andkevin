6a562e3716337a76c291768070ce53c675f61fea
asus <asus@DESKTOP-0O0BO62.dhcp.wustl.edu>
2018-01-17 17:21:18 -0600

    more changes

---
+++ heartbeatKA/heartbeat/heartbeat.ino
@@ -0,0 +1,15 @@
+int n =0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+   delay(1000);
+    n += 1;
+    Serial.print(n);
+    digitalWrite(13, HIGH);
+    Serial.println(" sec have elapsed");  
+    digitalWrite(13, LOW);
+}
