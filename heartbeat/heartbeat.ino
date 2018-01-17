3f298c5bf31f414b5b6c7047bf93c8f7974077b3
Daniel Smits <Daniel Smits@DanielSmitsPC.dhcp.wustl.edu>
2018-01-17 17:20:08 -0600

    Finalllllyyyy

---
+++ heartbeat/heartbeat.ino
@@ -0,0 +1,16 @@
+int n =0;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+ double t = millis()/1000.0;
+ Serial.print(t);
+ Serial.println(" millisecs passed.");
+ n=n+1;
+ delay(1000);
+ Serial.print(n);
+ Serial.println(" heartbeats.");
+}
+
